5606abd68c54f57a3b0fbf676c63d8a1c92c81c9
Lautaro005 <lautarosilvasikora@gmail.com>
2025-12-06 00:58:56 -0300

    v. 1.2.2.02

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_6FJroruI.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({ runtime: 'nodejs22.x' }),
   vite: {
     plugins: [tailwindcss()]
   }
